5dd6bb5169984b9ab0b333fe0896e2101f770d83
Yaroslav Vershinin <vershininyn@yandex.ru>
2023-06-22 12:48:45 +0700

    back-end: v0.2

--- src/test/java/dev/projects/profsouz/opcuaclient/modules/IndexControllerModulesTests.java
+++ src/test/java/dev/projects/profsouz/opcuaclient/modules/IndexControllerModulesTests.java
@@ -1,22 +1,31 @@
 package dev.projects.profsouz.opcuaclient.modules;
 
-import dev.projects.profsouz.opcuaclient.controller.IndexController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IndexControllerModulesTests {
     @Autowired
-    private IndexController usersController;
+    private MockMvc mockMvc;
 
     @Test
     public void getIndexPageTest() throws Exception {
-        ModelAndView modelAndView = usersController.getDefaultData();
-
-        assertTrue(modelAndView.getStatus().is2xxSuccessful());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/")
+                        .contentType(MediaType.TEXT_HTML_VALUE))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is2xxSuccessful());
     }
 }
